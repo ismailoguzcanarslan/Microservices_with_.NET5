9bf73685ea999a004ce18d72535eff0849a3711d
İsmail Oğuzcan Arslan <ismailoguzcanarslan@gmail.com>
2022-10-25 03:03:34 +0300

    Identity Server user manager controller development

---
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using FreeCourse.IdentityServer.Dtos;
+using FreeCourse.IdentityServer.Models;
+using FreeCourse.Shared.ControllerBasic;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : CustomBaseController
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupDto signupDto)
+        {
+            var isUserExist = await _userManager.FindByEmailAsync(signupDto.Email);
+
+            if(isUserExist != null)
+                return BadRequest(Response<IdentityError>.Error("User Exists", 500));
+
+            var user = new ApplicationUser()
+            {
+                UserName = signupDto.Username,
+                Email = signupDto.Email,
+                City = signupDto.City
+            };
+
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
+
+            if (!result.Succeeded)
+                return BadRequest(Response<IdentityError>.Error(result.Errors.Select(a=>a.Description).ToList(),500));
+            return NoContent();
+        }
+    }
+}
