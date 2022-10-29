e252dc8cbce021c1b094ce09df1c704c85c46c11
İsmail Oğuzcan Arslan <ismailoguzcanarslan@gmail.com>
2022-10-30 02:43:31 +0300

    Basket API added

---
+++ Services/Basket/FreeCourse.Services.Basket/Services/BasketService.cs
@@ -0,0 +1,40 @@
+﻿using FreeCourse.Services.Basket.Dtos;
+using FreeCourse.Shared.Dtos;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Basket.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+        public async Task<Response<bool>> Delete(string userId)
+        {
+            var status = await _redisService.GetDatabase().KeyDeleteAsync(userId);
+
+            return status ? Response<bool>.Success(204) : Response<bool>.Error("Basket Could Not Found", 500);
+        }
+
+        public async Task<Response<BasketDto>> GetBasket(string userId)
+        {
+            var isExistedBasket = await _redisService.GetDatabase().StringGetAsync(userId);
+
+            if (string.IsNullOrEmpty(isExistedBasket))
+                return Response<BasketDto>.Error("Basket Not Found", 404);
+            return Response<BasketDto>.Success(JsonSerializer.Deserialize<BasketDto>(isExistedBasket), 200);
+        }
+
+        public async Task<Response<bool>> SaveOrUpdate(BasketDto basketDto)
+        {
+            var status = await _redisService.GetDatabase().StringSetAsync(basketDto.UserId, JsonSerializer.Serialize(basketDto));
+
+            return status ? Response<bool>.Success(204) : Response<bool>.Error("Basket Could Not Update Or Save", 500);
+        }
+    }
+}
---
+++ Services/Basket/FreeCourse.Services.Basket/Services/RedisService.cs
@@ -0,0 +1,23 @@
+﻿using StackExchange.Redis;
+using System.Security.Principal;
+
+namespace FreeCourse.Services.Basket.Services
+{
+    public class RedisService
+    {
+        private readonly string _host;
+        private readonly int _port;
+        private ConnectionMultiplexer _multiplexer;
+
+
+        public RedisService(string host, int port)
+        {
+            _host = host;
+            _port = port;
+        }
+
+        public void Connect() => _multiplexer = ConnectionMultiplexer.Connect($"{_host}:{_port}");
+
+        public IDatabase GetDatabase (int db = 1) => _multiplexer.GetDatabase(db);
+    }
+}
