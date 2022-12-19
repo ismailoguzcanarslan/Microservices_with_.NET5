b7f79f4b61d38fe2913173ebd989feb30772bf16
İsmail Oğuzcan Arslan <ismailoguzcanarslan@gmail.com>
2022-12-19 20:54:42 +0300

    basket viewmodels added.

---
+++ FrontEnds/FreeCourse.Web/Models/BasketItemViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace FreeCourse.Web.Models
+{
+    public class BasketItemViewModel
+    {
+        public int Quantity { get; set; }
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public decimal Price { get; set; }
+        private decimal? DiscountAppliedPrice { get; set; } 
+
+        public decimal GetCurrentPrice
+        {
+            get => DiscountAppliedPrice != null ? DiscountAppliedPrice.Value : Price;
+        }
+
+        public void AppliedDiscount(decimal discountPrice)
+        {
+            DiscountAppliedPrice = discountPrice;
+        }
+    }
+}
---
+++ FrontEnds/FreeCourse.Web/Models/BasketViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FreeCourse.Web.Models
+{
+    public class BasketViewModel
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        private List<BasketItemViewModel> _basketitems { get; set; }
+
+        public List<BasketItemViewModel> BasketItems
+        {
+            get
+            {
+                if (HasDiscount)
+                {
+                    _basketitems.ForEach(x =>
+                    {
+                        var discountPrice = x.Price * ((decimal)DiscountRate.Value / 100);
+                        x.AppliedDiscount(Math.Round(x.Price - discountPrice, 2));
+                    });
+                }
+                return _basketitems;
+            }
+            set
+            {
+                _basketitems = value;
+            }
+        }
+
+        public decimal TotalPrice { get => _basketitems.Sum(a => a.GetCurrentPrice); }
+        public int? DiscountRate { get; set; }
+
+        public bool HasDiscount
+        {
+            get => !string.IsNullOrEmpty(DiscountCode);
+        }
+    }
+}
