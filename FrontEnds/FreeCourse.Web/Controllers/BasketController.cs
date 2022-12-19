c13cd6149d417fb55eb8160f21d38e6ba240f7f5
İsmail Oğuzcan Arslan <ismailoguzcanarslan@gmail.com>
2022-12-20 01:01:05 +0300

    basket bug fix.

--- FrontEnds/FreeCourse.Web/Controllers/BasketController.cs
+++ FrontEnds/FreeCourse.Web/Controllers/BasketController.cs
@@ -1,49 +1,49 @@
-﻿//using FreeCourse.Web.Models;
-//using FreeCourse.Web.Services.Interfaces;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.IdentityModel.Tokens;
-//using System.Threading.Tasks;
+﻿using FreeCourse.Web.Models;
+using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Threading.Tasks;
 
-//namespace FreeCourse.Web.Controllers
-//{
-//    public class BasketController : Controller
-//    {
-//        private readonly ICatalogService _catalogService;
-//        private readonly IBasketService _basketService;
+namespace FreeCourse.Web.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
 
-//        public BasketController(ICatalogService catalogService, IBasketService basketService)
-//        {
-//            _catalogService = catalogService;
-//            _basketService = basketService;
-//        }
+        public BasketController(ICatalogService catalogService, IBasketService basketService)
+        {
+            _catalogService = catalogService;
+            _basketService = basketService;
+        }
 
-//        public async Task<IActionResult> Index()
-//        {
-//            return View(await _basketService.Get());
-//        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _basketService.Get());
+        }
 
-//        public async Task<IActionResult> AddBasketItem(string courseId)
-//        {
-//            var course = await _catalogService.GetCourseByIdAsync(courseId);
+        public async Task<IActionResult> AddBasketItem(string courseId)
+        {
+            var course = await _catalogService.GetCourseByIdAsync(courseId);
 
-//            var basketItemViewModel = new BasketItemViewModel()
-//            {
-//                CourseId = courseId,
-//                CourseName = course.Name,
-//                Price = course.Price,
-//                Quantity = 1
-//            };
+            var basketItemViewModel = new BasketItemViewModel()
+            {
+                CourseId = courseId,
+                CourseName = course.Name,
+                Price = course.Price,
+                Quantity = 1
+            };
 
-//            await _basketService.AddBasketItem(basketItemViewModel);
+            await _basketService.AddBasketItem(basketItemViewModel);
 
-//            return RedirectToAction(nameof(Index));
-//        }
+            return RedirectToAction(nameof(Index));
+        }
 
-//        public async Task<IActionResult> RemoveBasketItem(string courseId)
-//        {
-//            await _basketService.RemoveBasketItem(courseId);
+        public async Task<IActionResult> RemoveBasketItem(string courseId)
+        {
+            await _basketService.RemoveBasketItem(courseId);
 
-//            return RedirectToAction(nameof(Index));
-//        }
-//    }
-//}
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
